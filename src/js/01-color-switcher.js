8b6753cb5c4de853857f3f6cca9fb32fbdf97338
Freebo12 <Tumechko12@gmail.com>
2023-01-12 18:27:39 +0100

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+//Dom Elements
+const btnJsStart = document.querySelector('button[data-start]');
+const btnJsStop = document.querySelector('button[data-stop]');
+const bodyJs = document.querySelector('body');
 
+let timerId = null;
+
+//function randomColor
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+//event click
+btnJsStart.addEventListener('click', onClick);
+btnJsStop.addEventListener('click', offClick);
+btnJsStop.disabled = true;
+
+//Start_click
+function onClick() {
+  timerId = setInterval(() => {
+    bodyJs.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  if ((btnJsStart.disabled = true)) {
+    btnJsStop.disabled = false;
+  }
+}
+//Stop_click
+function offClick() {
+  if ((btnJsStart.disabled = false)) {
+    btnJsStop.disabled = true;
+  }
+  btnJsStop.disabled = true;
+  clearInterval(timerId);
+}
