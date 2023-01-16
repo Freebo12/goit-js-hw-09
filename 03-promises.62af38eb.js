a7e9ce99bda2872c871ea723a52b1698f8f9d7a5
Freebo12 <110335199+Freebo12@users.noreply.github.com>
2023-01-16 14:45:54 +0000

    Deploying to gh-pages from @ Freebo12/goit-js-hw-09@789e85cfb22e96e127ef8c178b26f79c637ce71b 🚀

--- 03-promises.62af38eb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("submit",(function(o,e){Math.random()>.3?console.log("✅ Fulfilled promise ".concat(o," in ").concat(e,"ms")):console.log("❌ Rejected promise ".concat(o," in ").concat(e,"ms"))}));
-//# sourceMappingURL=03-promises.62af38eb.js.map
--- 03-promises.d4748c0a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("submit",(function(e,o){Math.random()>.3?console.log(`✅ Fulfilled promise ${e} in ${o}ms`):console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.d4748c0a.js.map
